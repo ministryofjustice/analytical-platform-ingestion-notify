6f36369fabbfdca60be5a32c66e656b8aed8c9bc
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-03-14 16:11:59 +0000

    Commit updated handler
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- src/var/task/handler.py
+++ src/var/task/handler.py
@@ -1,48 +1,52 @@
+import json
+import os
+
 import boto3
-import botocore.exceptions
 from notifications_python_client.notifications import NotificationsAPIClient
 
-notifications_client = NotificationsAPIClient("API_KEY")
-NOTIFICATIONS_TEMPLATE_ID = ""
-NOTIFICATION_EMAIL_ADDRESS = ""
-
 sm_client = boto3.client("secretsmanager")
+govuk_notify_api_key_secret = os.environ["GOVUK_NOTIFY_API_KEY_SECRET"]
+govuk_notify_api_key = sm_client.get_secret_value(SecretId=govuk_notify_api_key_secret)[
+    "SecretString"
+]
+govuk_notify_templates_secret = os.environ["GOVUK_NOTIFY_TEMPLATES_SECRET"]
+govuk_notify_templates = json.loads(
+    sm_client.get_secret_value(SecretId=govuk_notify_templates_secret)["SecretString"]
+)
+notifications_client = NotificationsAPIClient(govuk_notify_api_key)
 
 
 def handler(event, context):
     print("Received event:", event)
     print("Received context:", context)
-    message = event["Records"][0]["Sns"]["Message"]
-    state, object_key, time_stamp = message.split(",")[:3]
-    print(f"State: {state}")
-    print(f"Object key: {object_key}")
-    print(f"Time stamp: {time_stamp}")
 
-    if state == "infected":
-        print("Infected file detected")
-    elif state == "synced":
-        print("File synced")
+    s3_bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    object_key = event["Records"][0]["s3"]["object"]["key"]
 
-    try:
-        supplier, file_name = object_key.split("/")[:2]
-        print(f"Supplier: {supplier}")
-        print(f"File name: {file_name}")
+    supplier, file_name = object_key.split("/")[:2]
+    print(f"Supplier: {supplier}")
+    print(f"File name: {file_name}")
 
+    supplier_data_contact = sm_client.get_secret_value(
+        SecretId=f"ingestion/sftp/{supplier}/data-contact"
+    )["SecretString"]
+    # supplier_data_owner = sm_client.get_secret_value(
+    #     SecretId=f"ingestion/sftp/{supplier}/technical-contact"
+    # )["SecretString"]
+
+    if s3_bucket == "analytical-platform-quarantine":
+        print("File quarantined")
+        # send data owner
+        # notifications_client.send_email_notification(
+        #     template_id=govuk_notify_templates["sftp_quarantined_file_data_owner"],
+        #     email_address=supplier_data_owner,
+        #     personalisation={"supplier": supplier, "filename": file_name},
+        # )
+        # send data contact
         notifications_client.send_email_notification(
-            template_id=NOTIFICATIONS_TEMPLATE_ID,
-            email_address=NOTIFICATION_EMAIL_ADDRESS,
-            personalisation={"username": supplier, "filename": file_name},
+            template_id=govuk_notify_templates["sftp_quarantined_file_supplier"],
+            email_address=supplier_data_contact,
+            personalisation={"filename": file_name},
         )
-    except Exception as e:
-        print(f"Error: {e}")
-        raise e
-
-    try:
-        notifications_client.send_email_notification(
-            template_id=NOTIFICATIONS_TEMPLATE_ID,
-            email_address=NOTIFICATION_EMAIL_ADDRESS,
-            personalisation={"username": supplier, "filename": file_name},
-        )
-    except Exception as e:
-        print(f"Error: {e}")
-        raise e
+    else:
+        print(s3_bucket)
